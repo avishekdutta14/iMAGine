99a0222f8b67b0811e30c1c5a0a93b2214f276a0
Avishek Dutta <35264497+avishekdutta14@users.noreply.github.com>
2021-06-30 20:22:36 -0700

    In script description

--- imagine.sh
+++ imagine.sh
@@ -23,11 +23,17 @@ done
 
 START=$(date +%s)
 
+## filtering step
+
 fastp -i ${read1} -I ${read2} -o ${name}_filt_R1.fq.gz -O ${name}_filt_R2.fq.gz -e 30 -w 1
+
+## assembling step
 
 metaspades.py -1 ${name}_filt_R1.fq.gz -2 ${name}_filt_R2.fq.gz -k 21,33,55 -o metaspades_output_${name} -t 20 -m 300 
 
 quast.py metaspades_output_${name}/contigs.fasta -o quast_output
+
+## binning step
 
 mkdir binning
 
@@ -46,6 +52,8 @@ samtools sort -o ${name}_map_sorted.bam -O bam ${name}_map.bam
 jgi_summarize_bam_contig_depths --outputDepth ${name}_depth.txt ${name}_map_sorted.bam 
 
 metabat2 -i contigs.fasta -a ${name}_depth.txt -o bins_dir/${name}_bin --seed 1234 -m 1500 
+
+## checking bin quality
 
 checkm lineage_wf bins_dir/ checkm/ -x .fa -t 25 
 
